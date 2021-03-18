b92b3566ef94c8bedb094f519dd9b326e1bb41a5
balrom1981 <balrom@bk.ru>
2021-03-18 18:33:11 +0300

    Third test added

--- src/test/java/web/test/MoneyTransferTest.java
+++ src/test/java/web/test/MoneyTransferTest.java
@@ -48,5 +48,23 @@ class MoneyTransferTest {
     assertEquals((balanceSecondBillBeforeTransfer-amount),balanceSecondBillAfterTransfer);
   }
 
+  @Test
+  void shouldTransferMoneyFromSecondToFirstAmountBiggerThanBill() {
+    open("http://localhost:9999");
+    val loginPage = open("http://localhost:9999", LoginPageV2.class);
+    val authInfo = DataHelper.getAuthInfo();
+    val verificationPage = loginPage.validLogin(authInfo);
+    val verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+    val dashboardPage = verificationPage.validVerify(verificationCode);
+    val balanceFirstBillBeforeTransfer = dashboardPage.getFirstCardBalance();
+    val balanceSecondBillBeforeTransfer = dashboardPage.getSecondCardBalance();
+    val moneyTransferPage = dashboardPage.firstBill();
+    int amount = 100000;
+    moneyTransferPage.transferMoney(amount, DataHelper.Card.getCardSecond());
+    val balanceFirstBillAfterTransfer = dashboardPage.getFirstCardBalance();
+    val balanceSecondBillAfterTransfer = dashboardPage.getSecondCardBalance();
+    assertEquals((balanceFirstBillBeforeTransfer+amount),balanceFirstBillAfterTransfer);
+    assertEquals((balanceSecondBillBeforeTransfer-amount),balanceSecondBillAfterTransfer);
+  }
 }
 
